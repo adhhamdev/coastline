c2a6fef0c327973db665f1d3df986b227c7d04f2
Adhham Safwan <msmadhhamg@gmail.com>
2025-01-01 19:48:36 +0530

    add app redirect

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,12 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
-  const userAgent = request.headers.get('user-agent') || '';
-  
-  // Check if the request is from a mobile app
-  const isMobileApp = userAgent.toLowerCase().includes('coastlineapp');
-  console.log(userAgent, isMobileApp);
-  
+
   if (code) {
     const supabase = createClient();
     const { error, data } = await supabase.auth.exchangeCodeForSession(code);
@@ -18,12 +13,7 @@ export async function GET(request: Request) {
         data: { google_provider_token: data.session.provider_token },
       });
 
-      // If it's a mobile app request, redirect to the app's URL scheme
-      if (isMobileApp) {
-        return NextResponse.redirect(`coastlineapp.https://coastlineapp.vercel.app`);
-      }
-
-      return NextResponse.redirect(`${origin}/feed`);
+      return NextResponse.redirect(`coastlineapp.${origin}/feed`);
     }
   }
 
