ee5d6af31f9200ed29dd4bf5f5a87cb94a1f22da
Adhham Safwan <msmadhhamg@gmail.com>
2025-01-01 19:32:59 +0530

    add agent log

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,10 +5,10 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
   const userAgent = request.headers.get('user-agent') || '';
-  const isNativeApp = searchParams.get('isNativeApp');
   
   // Check if the request is from a mobile app
   const isMobileApp = userAgent.toLowerCase().includes('coastlineapp');
+  console.log(userAgent, isMobileApp);
   
   if (code) {
     const supabase = createClient();
@@ -19,7 +19,7 @@ export async function GET(request: Request) {
       });
 
       // If it's a mobile app request, redirect to the app's URL scheme
-      if (isMobileApp || isNativeApp) {
+      if (isMobileApp) {
         return NextResponse.redirect(`coastlineapp.https://coastlineapp.vercel.app`);
       }
 
