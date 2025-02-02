2c5d444dc34f9b003d8274909e513eb5a01850d3
Adhham Safwan <msmadhhamg@gmail.com>
2025-02-03 03:07:57 +0530

    fix path typo

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/common/Footer";
 import CTA from "@/components/pages/homepage/CTA";
 import Features from "@/components/pages/homepage/Features";
 import Hero from "@/components/pages/homepage/Hero";
-import HowItWorks from "@/components/pages/homepage/how-It-Works";
+import HowItWorks from "@/components/pages/homepage/how-It-works";
 import Partners from "@/components/pages/homepage/Partners";
 import Statistics from "@/components/pages/homepage/Statistics";
 import Testimonials from "@/components/pages/homepage/Testimonials";
