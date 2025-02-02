a20dbee646886942a4fd156b6ddb32e67fc060bd
Adhham Safwan <msmadhhamg@gmail.com>
2025-02-03 02:39:39 +0530

    fix path naming case

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/common/Footer";
 import CTA from "@/components/pages/homepage/cta";
 import Features from "@/components/pages/homepage/features";
 import Hero from "@/components/pages/homepage/hero";
-import HowItWorks from "@/components/pages/homepage/how-It-works";
+import HowItWorks from "@/components/pages/homepage/how-it-works";
 import Partners from "@/components/pages/homepage/partners";
 import Statistics from "@/components/pages/homepage/statistics";
 import Testimonials from "@/components/pages/homepage/testimonials";
